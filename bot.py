816f89b1d4b9c710c0ab21ec21f4506938ee8dbc
Mano Ranjan G <official.manoranjanworks@gmail.com>
2026-01-14 12:17:46 +0530

    Initial Commit

--- bot.py
+++
@@ -1,4 +0,0 @@
-from telegram import Bot
-
-bot = Bot(token="https://api.telegram.org/bot8098658072:AAE6T4G2uW10J98iv1S_SyQNF3A8PrEbEUo/getUpdates")
-bot.send_message(chat_id="1705351796", text="Test message")
